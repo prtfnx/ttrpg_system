f43fb992c2bfdd4c7449578a07fc6be7b5c42461
prtfnx <prtfnx@gmail.com>
2025-06-10 22:59:46 +0300

    Load SECRET_KEY and ALGORITHM from environment variables in Settings class

--- server_host/config.py
+++ server_host/config.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 
 class Settings(BaseSettings):
     """Settings for the application, loaded from environment variables."""
    
-    SECRET_KEY: str 
-    ALGORITHM: str 
+    # These will be read from environment variables set manually on Render
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "setted in env")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    
     model_config = SettingsConfigDict(env_file=".env")

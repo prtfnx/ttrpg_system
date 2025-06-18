df4cb7dbaa3ed5ef3b438361c24080b64bbb73ac
prtfnx <prtfnx@gmail.com>
2025-06-18 20:02:34 +0300

    lazy load StorageManager, reducing SDL3 dependency

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,10 +1,13 @@
 # Storage system for TTRPG
-from .storage_manager import StorageManager, get_storage_manager
 from .r2_manager import R2AssetManager
 
+# Lazy import of StorageManager to avoid SDL3 import on server
+def get_storage_manager():
+    """Lazy import of StorageManager to avoid SDL3 dependency on server"""
+    from .storage_manager import StorageManager, get_storage_manager as _get_storage_manager
+    return _get_storage_manager()
 
 __all__ = [
-    'StorageManager', 'get_storage_manager',
+    'get_storage_manager',
     'R2AssetManager', 
-    
 ]

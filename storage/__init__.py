4836ce473eddb07bda76e16521f106f3dd2514ab
prtfnx <prtfnx@gmail.com>
2025-06-25 19:39:15 +0300

    refactor: Remove lazy import of StorageManager to simplify storage module

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,13 +1,6 @@
 # Storage system for TTRPG
 from .r2_manager import R2AssetManager
 
-# Lazy import of StorageManager to avoid SDL3 import on server
-def get_storage_manager():
-    """Lazy import of StorageManager to avoid SDL3 dependency on server"""
-    from .storage_manager import StorageManager, get_storage_manager as _get_storage_manager
-    return _get_storage_manager()
-
-__all__ = [
-    'get_storage_manager',
+__all__ = [    
     'R2AssetManager', 
 ]

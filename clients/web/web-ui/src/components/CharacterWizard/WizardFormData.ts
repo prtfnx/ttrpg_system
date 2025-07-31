19a0a8a0fcf65bc647c3944bc2ccf5bbd3a18adb
prtfnx <prtfnx@gmail.com>
2025-07-31 16:49:29 +0300

    Add WizardFormData type: define structure for character wizard form data

---
+++ clients/web/web-ui/src/components/CharacterWizard/WizardFormData.ts
@@ -0,0 +1,15 @@
+export type WizardFormData = {
+  race: string;
+  class: string;
+  background: string;
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  skills: string[];
+  name: string;
+  bio?: string;
+  image?: string;
+};

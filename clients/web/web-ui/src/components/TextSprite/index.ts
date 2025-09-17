6a346942b5750de9fbca2516b1e8f754dbd2e753
prtfnx <prtfnx@gmail.com>
2025-09-17 23:56:59 +0300

    feat: create index file for TextSprite components and utilities exports

---
+++ clients/web/web-ui/src/components/TextSprite/index.ts
@@ -0,0 +1,10 @@
+export { TextSpriteCreator, type TextSpriteConfig } from './TextSpriteCreator';
+export { TextSpriteTool } from './TextSpriteTool';
+export { 
+  createTextSprite, 
+  updateTextSprite, 
+  deleteTextSprite, 
+  renderTextSprite,
+  type TextSpriteData,
+  type RenderResult 
+} from './textSpriteUtils';

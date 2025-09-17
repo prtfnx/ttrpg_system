d3b30981e6d84c9e70e8e480d8c67098c83fe5c0
prtfnx <prtfnx@gmail.com>
2025-09-17 23:58:26 +0300

    fix: reorder export of TextSpriteTool for consistency in index file

--- clients/web/web-ui/src/components/TextSprite/index.ts
+++ clients/web/web-ui/src/components/TextSprite/index.ts
@@ -1,7 +1,8 @@
 export { TextSpriteCreator, type TextSpriteConfig } from './TextSpriteCreator';
-export { TextSpriteTool } from './TextSpriteTool';
 export { TextSpriteEditor } from './TextSpriteEditor';
+export { TextSpriteTool } from './TextSpriteTool';
 export {
     createTextSprite, deleteTextSprite,
     renderTextSprite, updateTextSprite, type RenderResult, type TextSpriteData
 } from './textSpriteUtils';
+

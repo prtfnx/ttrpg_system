e2515a9f404ac7ad5fa58498f95d9cf2cdf0bd64
prtfnx <prtfnx@gmail.com>
2025-09-19 22:18:25 +0300

    refactor: reorder import statements in SpellManager component

--- clients/web/web-ui/src/components/CharacterWizard/SpellManager.tsx
+++ clients/web/web-ui/src/components/CharacterWizard/SpellManager.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './SpellManager.css';
 import type { WizardFormData } from './WizardFormData';
 

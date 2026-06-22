b5ef462f1d9d076dde1fbc8d51c5a97bfcde1515
prtfnx <prtfnx@gmail.com>
2026-06-22 15:04:38 +0300

    refactor(auth): delete legacy auth client flow that are no more used

--- apps/web-ui/src/features/auth/index.ts
+++ apps/web-ui/src/features/auth/index.ts
@@ -1,11 +1,5 @@
 export { AuthProvider, useAuth } from '@app/providers';
-export { default as AuthGuard } from './components/AuthGuard';
-export { default as EnhancedLogin } from './components/EnhancedLogin';
-export { default as LoginModal } from './components/LoginModal';
-export { default as UserMenu } from './components/UserMenu';
 
 export { useAuthenticatedWebSocket } from './hooks/useAuthenticatedWebSocket';
 
 export { authService, type SessionInfo, type UserInfo } from './services/auth.service';
-export { enhancedAuthService } from './services/enhancedAuth.service';
-
